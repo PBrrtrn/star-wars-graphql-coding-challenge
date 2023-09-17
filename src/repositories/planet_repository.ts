8284825e19ba466bddef433a27420e5c3a976f9c
Pablo Berrotaran <pberrotaran@keepcon.com>
2023-09-16 21:19:45 -0300

    Index PlanetRepository data by ID for faster access

--- src/repositories/planet_repository.ts
+++ src/repositories/planet_repository.ts
@@ -5,25 +5,28 @@ export class PlanetRepository {
     private static instance: PlanetRepository = this.createInstance();
 
     private static createInstance(): PlanetRepository {
-        return new PlanetRepository([]);
+        return new PlanetRepository({});
     }
 
-    private constructor(private data: Planet[]) {};
+    private constructor(private data: {[key: number]: Planet}) {};
+
+    private currentId: number = 0;
 
     public static getInstance(): PlanetRepository {
         return this.instance;
     };
 
     public getAll(): Planet[] {
-        return this.data;
+        return Object.values(this.data);
     }
 
     public insert(planet: Planet) {
-        this.data.push(planet);
+        this.data[this.currentId] = planet;
+        this.currentId++;
     }
 
     public clear() {
         // TODO: Raise exception if ENV is not test
-        this.data = [];
+        this.data = {};
     }
 }
