83c250b1c182b2cb651fa3f7018bf9d18cb260bb
Pablo Berrotaran <pberrotaran@keepcon.com>
2023-09-16 21:02:11 -0300

    Further cleanup

--- src/resolvers/planet_resolver.ts
+++ src/resolvers/planet_resolver.ts
@@ -4,7 +4,6 @@ import { PlanetSerializer } from "../serializers/planet_serializer";
 export const planetResolver = {
     Query: {
         planets() {
-            // return [{name: "Pepe", population: 1, terrain: "pepe", climate: "pepe", latitude: 0.0, longitude: 0.0 }]
             return PlanetRepository.getInstance().getAll().map(planet => {
                 return PlanetSerializer.serialize(planet);
             });
